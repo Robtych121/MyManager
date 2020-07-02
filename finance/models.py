9cef937c3f4326e9160f20143dddb38366ff731c
Robert Davies <rob.davies1991@gmail.com>
2020-07-02 21:46:57 +0100

    created accounts modal and view page

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Account(models.Model):
+
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    name = models.CharField(max_length=254, default='')
+    description = models.CharField(max_length=254, default='')
+    balance = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    currency = models.CharField(max_length=254, default='')
+
+    def __str__(self):
+        return self.name
