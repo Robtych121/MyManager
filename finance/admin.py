fddcc17c2c55ada6ce3ea91a62fb0bc119bcf2d1
Robert Davies <rob.davies1991@gmail.com>
2020-07-09 09:18:05 +0100

    created transcation and related models

--- finance/admin.py
+++ finance/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Account
+from .models import Account, Period, Category, Type, Transcation
 
 # Register your models here.
 admin.site.register(Account)
+admin.site.register(Period)
+admin.site.register(Category)
+admin.site.register(Type)
+admin.site.register(Transcation)
--- finance/models.py
+++ finance/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from datetime import datetime
+import calendar
 
 # Create your models here.
 class Account(models.Model):
@@ -15,3 +16,65 @@ class Account(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Period(models.Model):
+    firstDayOfMonth = datetime.today().replace(day=1)
+    now = datetime.now()
+    lastDayOfMonth = datetime.today().replace(day=calendar.monthrange(now.year,now.month)[1])
+
+
+    class Meta:
+        verbose_name = 'Period'
+        verbose_name_plural = 'Periods'
+
+    name = models.CharField(max_length=254, default='')
+    startDate = models.DateField(default=firstDayOfMonth)
+    endDate = models.DateField(default=lastDayOfMonth)
+    startBalance = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    endBalance = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    totalIn = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    totalOut = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    freeCash = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    STATUSCHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive')
+    )
+    status = models.CharField(max_length=254, choices=STATUSCHOICES, default='')
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254, default='')
+
+
+class Type(models.Model):
+    class Meta:
+        verbose_name = 'Type'
+        verbose_name_plural = 'Types'
+
+    name = models.CharField(max_length=254, default='')
+
+
+class Transcation(models.Model):
+    class Meta:
+        verbose_name = 'Transcation'
+        verbose_name_plural = 'Transcations'
+
+
+    name = models.CharField(max_length=254, default='')
+    value = models.DecimalField(default=0, max_digits=19, decimal_places=2)
+    period = models.ForeignKey(Period, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    STATUSCHOICES = (
+        ('Paid', 'Paid'),
+        ('Planned', 'Planned')
+    )
+    status = models.CharField(max_length=254, choices=STATUSCHOICES, default='')
+    notes = models.CharField(max_length=254, default='')
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
