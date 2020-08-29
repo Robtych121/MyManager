074c4d5558da81b132a4dc6accb7386a58ad19e3
Robert Davies <rob.davies1991@gmail.com>
2020-08-29 22:28:00 +0100

    created transcations form

--- finance/forms.py
+++ finance/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Account, Period
+from .models import Account, Period, Transcation
 import html5.forms.widgets as html5_widget
 
 class AccountForm(forms.ModelForm):
@@ -12,3 +12,9 @@ class PeriodForm(forms.ModelForm):
     class Meta:
         model = Period
         fields = ('name', 'startDate', 'endDate', 'startBalance', 'endBalance', 'status')
+
+
+class TranscationForm(forms.ModelForm):
+    class meta:
+        model = Transcation
+        fields = ('name', 'value', 'period', 'category', 'type', 'status', 'notes', 'account')
